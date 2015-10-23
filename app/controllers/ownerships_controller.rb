eafb06b3f83dfe0805b6ef84d2f5a8cace578b1e
birutosyubain <amarion733@yahoo.co.jp>
2015-10-23 14:31:24 +0000

    have_and_want_btn

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,25 +12,30 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
-      rescue Amazon::RequestError => e
-        return render :js => "alert('#{e.message}')"
-      end
+        response = Amazon::Ecs.item_lookup(params[:asin], :response_group => 'Medium',
+                                                          :country => 'jp')
+        rescue Amazon::RequestError => e
+          return render :js => "alert('#{e.message}')"
+        end
 
-      amazon_item       = response.items.first
-      @item.title        = amazon_item.get('ItemAttributes/Title')
-      @item.small_image  = amazon_item.get("SmallImage/URL")
-      @item.medium_image = amazon_item.get("MediumImage/URL")
-      @item.large_image  = amazon_item.get("LargeImage/URL")
-      @item.detail_page_url = amazon_item.get("DetailPageURL")
-      @item.raw_info        = amazon_item.get_hash
-      @item.save!
+        amazon_item       = response.items.first
+        @item.title        = amazon_item.get('ItemAttributes/Title')
+        @item.small_image  = amazon_item.get("SmallImage/URL")
+        @item.medium_image = amazon_item.get("MediumImage/URL")
+        @item.large_image  = amazon_item.get("LargeImage/URL")
+        @item.detail_page_url = amazon_item.get("DetailPageURL")
+        @item.raw_info        = amazon_item.get_hash
+        @item.save!
     end
 
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値ににHaveボタンが押された時には「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
-    
+    if params[:type] == "Have"
+      current_user.have(@item)
+    elsif params[:type] == "Want"
+      current_user.want(@item)
+    end
 
   end
 
@@ -40,6 +45,11 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時には「Have」,
     # Wantedボタンがされた時には「Want」が設定されています。
+    if params[:type] == "Haved"
+      current_user.unhave(@item)
+    elsif params[:type] == "Wanted"
+      current_user.unwant(@item)
+    end
 
   end
 end
